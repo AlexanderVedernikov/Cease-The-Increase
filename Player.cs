3696966711cc6fffcf6a66cf1e4496cfbf17e2d9
EmmetEmet <emmetemet8@gmail.com>
2024-02-08 14:12:54 -0500

    Character now turns

--- Player.cs
+++ Player.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
+using System.Numerics;
 
 public partial class Player : CharacterBody2D
 {
 	private float _runSpeed = 350;
     private float _jumpSpeed = -1000;
     private float _gravity = 2500;
+    private int _direction = 1;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -42,6 +44,10 @@ public partial class Player : CharacterBody2D
         var velocity = Velocity;
         velocity.Y += _gravity * (float)delta;
         Velocity = velocity;
+        if ((Mathf.Sign(velocity.X) != Mathf.Sign(_direction)) & (Mathf.Sign(velocity.X) != 0)){ //If the velocity and direction aren't the same, that means that the character has started moving in the opposite direction
+            ApplyScale(new Godot.Vector2(-1, 1));
+            _direction = _direction * -1;
+        }
         GetInput();
         MoveAndSlide();
     }
