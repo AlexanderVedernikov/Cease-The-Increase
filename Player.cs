472d746f372db95f73646ef49981e274566bca95
CodingLegend0100 <codinglegend0100@gmail.com>
2024-02-08 23:04:44 -0500

    Renamed variables for clarity

--- Player.cs
+++ Player.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Numerics;
 
+// Class defining player functionality
 public partial class Player : CharacterBody2D
 {
 	private float _runSpeed = 350;
@@ -22,29 +23,29 @@ public partial class Player : CharacterBody2D
 
     public void GetInput()
     {
-        var velocity = Velocity;
-        velocity.X = 0;
+        var newVel = Velocity;
+        newVel.X = 0;
 
         var right = Input.IsActionPressed("ui_right");
         var left = Input.IsActionPressed("ui_left");
         var jump = Input.IsActionPressed("ui_up");
 
         if (IsOnFloor() && jump)
-            velocity.Y = _jumpSpeed;
+            newVel.Y = _jumpSpeed;
         if (right)
-            velocity.X += _runSpeed;
+            newVel.X += _runSpeed;
         if (left)
-            velocity.X -= _runSpeed;
+            newVel.X -= _runSpeed;
 
-        Velocity = velocity;
+        Velocity = newVel;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        var velocity = Velocity;
-        velocity.Y += _gravity * (float)delta;
-        Velocity = velocity;
-        if ((Mathf.Sign(velocity.X) != Mathf.Sign(_direction)) & (Mathf.Sign(velocity.X) != 0)){ //If the velocity and direction aren't the same, that means that the character has started moving in the opposite direction
+        var newVel = Velocity;
+        newVel.Y += _gravity * (float)delta;
+        Velocity = newVel;
+        if ((Mathf.Sign(newVel.X) != Mathf.Sign(_direction)) & (Mathf.Sign(newVel.X) != 0)){ //If the velocity and direction aren't the same, that means that the character has started moving in the opposite direction
             ApplyScale(new Godot.Vector2(-1, 1));
             _direction = _direction * -1;
         }
